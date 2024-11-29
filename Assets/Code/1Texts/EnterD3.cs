2b2499a6243a4e11cfed7a54ea744942118ca799
Deniz852 <denisdvcube@gmail.com>
2024-11-29 18:33:06 +0500

    ,

---
+++ Assets/Code/1Texts/EnterD3.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterD3 : MonoBehaviour
+{
+    public string mess1 = "Text";
+    public string mess2 = "Text";
+    public string mess3 = "Text";
+
+    void OnTriggerEnter(Collider other)
+    {
+        print(mess1);
+        print(mess2);
+        print(mess3);
+
+    }
+}
