56b9f5b7c894b63231612e47e20098e194600ee0
Deniz852 <denisdvcube@gmail.com>
2025-02-15 13:32:27 +0500

    .

---
+++ Assets/Code/CutCollider.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutCollider : MonoBehaviour
+{
+    public GameObject cutScene;
+    private void Start()
+    {
+        cutScene.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Robot")
+        {
+            cutScene.SetActive(true);
+        }
+    }
+}
